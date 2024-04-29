634340484ea8b442533e93c7f1c506d40180935f
WHITE-R4V3N <emma.morgan.gillespie@gmail.com>
2024-04-29 14:49:11 -0600

    added mor test data

--- tokenizer.py
+++ tokenizer.py
@@ -11,20 +11,31 @@ import regex as re
 
 from datasets import load_dataset
 
-file_paths = ["data/test1.txt", "data/test2.txt", "data/test3.txt"]
+file_paths = ["data/test1.txt", "data/test2.txt", "data/test3.txt", "data/test4.txt", "data/test5.txt"]
 
 for file_path in file_paths:
     with open(file_path, "r", encoding="utf-8") as file:
         text = file.read()
 
         # Create specific tokens for flags, ip_address, ports
+        ip_addresses = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', text)
         text_token = re.sub(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', '<IP>', text)
+
+        flags = re.findall(r"\w+'?\w*\{[a-zA-Z0-9_'-]+\}", text_token)
         text_token = re.sub(r"\w+'?\w*\{[a-zA-Z0-9_'-]+\}", '<FLAG>', text_token)
 
-        ip_addresses = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', text)
-        flags = re.findall(r"\w+'?\w*\{[a-zA-Z0-9_'-]+\}", text)
+        port = re.findall(r'\b\d{1,5}\b', text_token)
+        text_token = re.sub(r'\b\d{1,5}\b', '<PORT>', text_token)
 
         print(text_token)
         print(f'IP: {ip_addresses}')
+        print(f'Port: {port}')
         print(f'Flag: {flags}\n----------------------------------------------\n')
+
+
+# Create a bunch of files based on Cyber security blog posts and along with many CTF challenges. This will fill out the
+# possible tokens that can appear. We will then use this to make a corpus based on inputs. We can then send data to be
+# tokenized and compared to the corpus. From there we can use this information tp get desired results from the server.
+# We can do this by running the test data through the tokenizer and saving the corpus created. We can then use that
+# to train the AI model and set the expected output. (ie. Scan and dirb, [1,0,0,0,0,0,1,...]).
 
