90a0eec1da8091c10487f0fdf0f6e66d8abc18d3
WHITE-R4V3N <emma.morgan.gillespie@gmail.com>
2025-01-31 19:23:32 -0700

    Tokenize, pad, and normalize the x input. Will fix code to one-hot encode y / keep y as tokenized and normalized. one-hot for the predictive model and normalized for the generative??? Still haven't figured that part out quite yet.

--- calista.py
+++ calista.py
@@ -1,6 +1,6 @@
 # Author:       Emma Gillespie
 # Date:         2024-03-20
-# Description:  This file serves as the main file for IRIS. This file will handle
+# Description:  This file serves as the main file for Calista. This file will handle
 #               user input and give the neural network output in response. This includes
 #               running tests and or commands. All will be kept in a log file for documentation
 #               purposes. Name based on greek goddess for communication.
@@ -42,67 +42,27 @@ tokenizer.add_data_file(json.load(open('datasets/command_data.json', 'r')))
 # Parse the files
 training_data = tokenizer.parse_files()
 
-# Now we need to tokenize each string of data
-print(f'{training_data}\n')
-'''
-X = []
-for i in training_data:
-    print(f'{training_data[i][0]}\n{training_data[i][1]}') # Can remove
+# Tokenize, Pad and Normalize the training data
+norm_x = tokenizer.tokenize_pad_normalize(tokenizer, training_data, 0)
+norm_y = tokenizer.tokenize_pad_normalize(tokenizer, training_data, 1)
 
-    data_x = ''
+for i in norm_x:
+    print(f'Input Size: {len(i)}')
 
-    for labels in training_data[i][0]:
-        data_x += f'{str(training_data[i][0][labels])} '
-
-    X.append(tokenizer.char_tokenize(data_x))
-    X_tokens = tokenizer.char_tokenize(data_x)
-
-    tokenizer.max_length = max(len(seq) for seq in X)
-    print(f'{X_tokens}\n\n')                                # Can remove
-
-padded_x = tokenizer.pad_input(X)
-print(padded_x)
-'''
-
-parsed_x = []
-for input_output in training_data:
-    x_string = ''
-    for labels in training_data[input_output][0]:
-        x_string += f'{training_data[input_output][0][labels]} '
-
-    parsed_x.append(x_string)
-
-tokenized_x = []
-for i in parsed_x:
-    tokenized_x.append(tokenizer.char_tokenize(i))
-
-padded_x = tokenizer.pad_input(tokenized_x)
-
-normalized_x = []
-for item in padded_x:
-    x_max = max(i for i in item)
-    normalized_x.append(tokenizer.normalize_array(item, x_max))
-
-print(f'{parsed_x}\n')
-print(f'{tokenized_x}\n')
-print(f'{padded_x}\n')
-print(f'{x_max}\n')
-print(f'{normalized_x}\n')
-
-# WHY THE FUCK IS NORMALIZING SO FUCKING HARD TO DO! LIKE FUCK
+# Output needs to be one-hot encoded. Or better yet it can be a fixed length and we pad the output
 
 #-----------------------------------------------------------------------------#
 # Create the AI models that will be used and train them on the training data. #
 #-----------------------------------------------------------------------------#
 
 # The input sizes will depend on the size of the X data in the training data.
-predictive_obj = Predictive_NN(input_size=64, hidden_size=1024, hidden2_size=1024, output_size=1)
+predictive_obj = Predictive_NN(input_size=353, hidden_size=1024, hidden2_size=1024, output_size=1)
 transformer_obj = Transformer(vocab_size=100, embed_size=32, num_heads=4, num_layers=2, feedforward_dim=64)
 
 # Need to create the input tokens here
-predicted_tokens = [5, 12]
+predicted_tokens = [5, 12] # Should be generated after each input from the user.
 usr_input_tokens = [20, 8]
-seed = predicted_tokens + usr_input_tokens
+seed = predicted_tokens + usr_input_tokens # What we are feeding into the transformer object
 
 output = generate_text(transformer_obj, seed, length=10, vocab_size=100)
 print(f'\nGenerated commands: \n{output}')
--- data_parse.py
+++ data_parse.py
@@ -28,11 +28,8 @@ class DataTokenizer:
         return [seq + [0] * (self.max_length - len(seq)) for seq in data_pad]
     
     # Normalizes the values (creates a decimal 0 = 0 and 1 = 65535)
-    def normalize_array(self, arr, max_value):
-        l = []
-        for item in arr:
-            item / max_value
-        return l
+    def normalize_array(self, digi, max_value):
+        return digi / max_value
     
     # Parses the inputs and outputs from the data file given
     def parse_files(self):
@@ -51,3 +48,30 @@ class DataTokenizer:
             id_value += 1
 
         return parsed_data
+    
+    def tokenize_pad_normalize(self, tokenizer, training_data, x_or_y):
+        tokenized_data = []
+        for input_output in training_data:
+            x_string = ''
+            for labels in training_data[input_output][x_or_y]: # The var changning the data changes this 0 and the 0 below to a variable instead
+                x_string += f'{training_data[input_output][x_or_y][labels]} '
+
+            tokenized_data.append(tokenizer.char_tokenize(x_string))
+
+        if x_or_y == 0:
+            padded_data = tokenizer.pad_input(tokenized_data)
+        else:
+            padded_data = tokenized_data
+
+        # Can be generalized to just return normalized_data
+        normalized_data = []
+        for item in padded_data:
+            data_max = max(i for i in item)
+            digi_arr = []
+
+            for digi in item:
+                digi_arr.append(tokenizer.normalize_array(digi, data_max))
+            
+            normalized_data.append(digi_arr)
+
+        return normalized_data
